c7ac2e00440df3b66df56ca9e06aec77a7eeefd9
Titus Fortner <titusfortner@gmail.com>
2017-08-16 13:12:08 -0500

    run in parallel on Sauce

--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -2,7 +2,10 @@ describe('The Modern Software Factory Demo UI Test - NA East', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://naeast-preprod.cdbu.io');
-    element(by.css('[name="name"]')).sendKeys('Selenium Test User');
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User ' + parseInt(Math.random() * (10000)));
     element(by.buttonText('I\'m In!')).click();
-    });
+    const EC = protractor.ExpectedConditions;
+    browser.wait(EC.urlContains('welcome'), 5000);
+    expect(element(by.css('.cross')).isPresent()).toBe(true);
+  });
 });
